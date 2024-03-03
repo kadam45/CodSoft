5fbe9591316397f45492d9d1c86272248dd562b6
Sanket Kadam <153412193+kadam45@users.noreply.github.com>
2024-03-03 10:57:47 +0530

    Add files via upload

---
+++ Web Development(Level 1)/Calculator/index.js
@@ -0,0 +1,34 @@
+const result = document.getElementById("result");
+const button = document.getElementsByTagName("button");
+const buttons = Array.from(button);
+let string = "";
+buttons.forEach(ele => {
+    ele.addEventListener("click", (e) => {
+        ele.classList.add('scaled');
+        setTimeout(() => {
+            ele.classList.remove('scaled');
+        }, 300);
+
+        if (e.target.innerHTML == "=") {
+            if(result.value==""){
+                result.value=""; 
+            }
+            else{
+                string = eval(string);
+                result.value = string;
+            }
+        }
+        else if (e.target.innerHTML == "AC") {
+            string = "";
+            result.value = string;
+        }
+        else if (e.target.innerHTML == "DEL") {
+            string = string.substring(0, string.length - 1);
+            result.value = string;
+        }
+        else {
+            string += e.target.innerHTML;
+            result.value = string;
+        }
+    });
+})
